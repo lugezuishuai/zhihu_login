7de314126a434302124108ca8c4bd0305cf5f1b2
lugezuishuai <984374450@qq.com>
2020-03-21 09:43:25 +0800

    add table_onblur

--- hide_eye.js
+++ hide_eye.js
@@ -1,6 +1,35 @@
 var appear = document.getElementById("appear");
 var hide = document.getElementById("hide");
+
 var pwd = document.getElementById("pwd");
+var username = document.getElementById("username");
+
+var pwd_blur = document.getElementById("pwd_blur");
+var username_blur = document.getElementById("username_blur");
+
+username.onblur = function()
+{
+    username.style.display = "none";
+    username_blur.style.display = "inline-block";
+}
+
+pwd.onblur = function()
+{
+    pwd.style.display = "none";
+    pwd_blur.style.display = "inline-block";
+}
+
+username_blur.onfocus = function()
+{
+    username_blur.style.display = "none";
+    username.style.display = "inline-block";
+}
+
+pwd_blur.onfocus = function()
+{
+    pwd_blur.style.display = "none";
+    pwd.style.display = "inline-block";
+}
 
 appear.onclick = function(){
     appear.style.display = "none";
@@ -14,8 +43,8 @@ hide.onclick = function(){
     pwd.type = "password";
 }
 
-var eye = document.getElementById("eye");
-var pwd = document.getElementById("pwd");
+// var eye = document.getElementById("eye");
+// var pwd = document.getElementById("pwd");
 
 // 方法二：
 // function showhide(){
