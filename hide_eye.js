76e2c30fcb6247a769ca038dab0f4ffd4791a6aa
lugezuishuai <984374450@qq.com>
2020-04-04 13:23:41 +0800

    modify the click and blur event.

--- hide_eye.js
+++ hide_eye.js
@@ -9,26 +9,36 @@ var username_blur = document.getElementById("username_blur");
 
 username.onblur = function()
 {
-    username.style.display = "none";
-    username_blur.style.display = "inline-block";
+    //如果原来的输入文本框中没有内容才会显示
+    if(!username.value){
+        username.style.display = "none";
+        username_blur.style.display = "inline-block";
+    }
 }
 
 pwd.onblur = function()
 {
-    pwd.style.display = "none";
-    pwd_blur.style.display = "inline-block";
+    //如果原来的输入文本框中没有内容才会显示
+    if(!pwd.value){
+        pwd.style.display = "none";
+        pwd_blur.style.display = "inline-block";
+    }
 }
 
 username_blur.onfocus = function()
 {
     username_blur.style.display = "none";
     username.style.display = "inline-block";
+    //原来的输入文本框立即获得焦点
+    username.focus();
 }
 
 pwd_blur.onfocus = function()
 {
     pwd_blur.style.display = "none";
     pwd.style.display = "inline-block";
+    //原来的输入文本框立即获得焦点
+    pwd.focus();
 }
 
 appear.onclick = function(){
